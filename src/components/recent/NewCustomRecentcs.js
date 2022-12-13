58ad002bdad4a302dccbb9bf055fdb9d9f344ab0
Jawad Raza <jawadraza8372@gmail.com>
2022-12-13 13:21:11 +0500

    debugging recentcs

--- src/components/recent/NewCustomRecentcs.js
+++ src/components/recent/NewCustomRecentcs.js
@@ -9,29 +9,31 @@ const NewCustomRecentcs = ({ name, tag, productData }) => {
   return (
     <div className="col-11" style={{ margin: "0px auto" }}>
       <h1>{tag}</h1>
-      <div className="productShowDiv" style={{ background: "red" }}>
-        {recentSlider?.slice(0, 7).map((elem, index) => {
-          const newname = makingValidName(`${elem.name}`);
-          const newshoeid = makingValidName(`${elem.shoe_id}`);
-          return (
-            <>
-              <Link to={`/${newname}_id_${newshoeid}`} key={index}>
-                <div className="cardDiv">
-                  <img
-                    className="cardImg"
-                    src={elem.cover_image ? elem.cover_image : cardImg6}
-                  />
-                  <div className="textCont">
-                    <h5>{elem.name}</h5>
-                    <span>Lowest Price</span>
-                    <h5>null AED</h5>
-                    <div className="lastSoldDiv">Last Sold: null</div>
+      <div className="row">
+        <div className="productShowDiv" style={{ background: "red" }}>
+          {recentSlider?.slice(0, 7).map((elem, index) => {
+            const newname = makingValidName(`${elem.name}`);
+            const newshoeid = makingValidName(`${elem.shoe_id}`);
+            return (
+              <>
+                <Link to={`/${newname}_id_${newshoeid}`} key={index}>
+                  <div className="cardDiv">
+                    <img
+                      className="cardImg"
+                      src={elem.cover_image ? elem.cover_image : cardImg6}
+                    />
+                    <div className="textCont">
+                      <h5>{elem.name}</h5>
+                      <span>Lowest Price</span>
+                      <h5>null AED</h5>
+                      <div className="lastSoldDiv">Last Sold: null</div>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </>
-          );
-        })}
+                </Link>
+              </>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
